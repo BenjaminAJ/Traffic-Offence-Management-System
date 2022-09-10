20c2d58495e0cb8b871cf665fdc53cf919187142
BenjaminAJ <ajanakub30@gmail.com>
2022-09-10 12:45:21 +0100

    Modified views for log in and log out.

--- PenaltyManager/views.py
+++ PenaltyManager/views.py
@@ -1,10 +1,11 @@
-from django.shortcuts import render
-
 # Importing required libraries
 from django.shortcuts import render
 from django.http import HttpResponse, JsonResponse
 from .models import Penalty, Driver
 from django.contrib.auth.models import User
+from .forms import LoginForm
+from django.shortcuts import render, redirect
+from django.utils.http import is_safe_url
 from django.contrib.auth import authenticate, login, logout
 from django.middleware import csrf
 import json
@@ -18,45 +19,40 @@ def home_page(request):
     return render(request, "base.html", context)
 
 
-def loginUser(request):
-    """Function for login view."""
-
-    # If GET request
-    if request.method == "GET":
-
-        # Returning response with login html page
-        response = render(request, "login.html")
-        return responseHeadersModifier(response)
-
-    # If POST request
-    elif request.method == "POST":
-
-        # Extracting data from POST request
-        username = request.POST["userName"]
-        password = request.POST["userPassword"]
-
-        # Authenticating username password
-        user = authenticate(request, username = username, password = password)
-
-        # If user authenticated successfully and user returned
+def login_page(request):
+    form = LoginForm(request.POST or None)
+    context = {
+        "form": form
+    }
+    next_ = request.GET.get('next')
+    next_post = request.POST.get('next')
+    redirect_path = next_ or next_post or None
+    # print(request.user.is_authenticated)
+    if form.is_valid():
+        # print(form.cleaned_data)
+        context["form"] = LoginForm()
+        username = form.cleaned_data.get("username")
+        password = form.cleaned_data.get("password")
+        user = authenticate(request, username=username, password=password)
+        print(request.user.is_authenticated)
         if user is not None:
-
-            # Logging in (session)
+            # print(request.user.is_authenticated)
             login(request, user)
-
-            # Returning response with index html page
-            response = render(request, "index.html")
-            return responseHeadersModifier(response)
-
-        # If no user found, user credentials wrong
+            try:
+                del request.session['guest_email_id']
+            except:
+                pass
+            if is_safe_url(redirect_path, request.get_host()):
+                return redirect(redirect_path)
+            else:
+            # A backend authenticated the credentials
+                return redirect("/")
         else:
+            # No backend authenticated the credentials
+            print("Error")
+    return render(request, "accounts/login.html", context)
 
-            # Failure message
-            context = {
-                "Failure": "Invalid credentials."
-            }
 
-            # Returning response
-            response = render(request, "login.html", context)
-            return responseHeadersModifier(response)
-
+def log_out(request):
+    logout(request)
+    return redirect("/")
