c5124deb513b57c5ccd355cb5ea15a2a9994cd58
BenjaminAJ <ajanakub30@gmail.com>
2022-09-11 01:02:33 +0100

    modified login html

--- PenaltyManager/urls.py
+++ PenaltyManager/urls.py
@@ -1,12 +1,11 @@
 from os import name
-from .views import home_page, log_out, login_request, dashboard_page
+from .views import log_out, login_request, dashboard_page
 from django.urls import path
 
 app_name = 'PenaltyManager'
 
 urlpatterns = [
-    path('', home_page, name='home'),
-    path('login/', login_request, name='login'),
+    path('', login_request, name='login'),
     path('logout/', log_out, name='logout'),
     path('dashboard/', dashboard_page, name='dashboard')
 ]
--- PenaltyManager/views.py
+++ PenaltyManager/views.py
@@ -12,64 +12,28 @@ from django.contrib.auth.forms import AuthenticationForm
 
 # Create your views here.
 
-def home_page(request):
-    context = {
-        
-    }
-    return render(request, "PenaltyManager/login.html", context)
-
 def dashboard_page(request):
     context = {
 
     }
     return render(request, "PenaltyManager/dashboard.html", context)
 
-def login_page(request):
-    form = LoginForm(request.POST or None)
-    context = {
-        "form": form
-    }
-    # print(request.user.is_authenticated)
-    if form.is_valid():
-        # print(form.cleaned_data)
-        context["form"] = LoginForm()
-        username = form.cleaned_data.get("username")
-        password = form.cleaned_data.get("password")
-        user = authenticate(request, username=username, password=password)
-        print(request.user.is_authenticated)
-        if user is not None:
-            # print(request.user.is_authenticated)
-            login(request, user)
-            try:
-                del request.session['guest_email_id']
-            except:
-                pass
-            if is_safe_url(redirect_path, request.get_host()):
-                return redirect(redirect_path)
+def login_request(request):
+    if request.method == "POST":
+        form = AuthenticationForm(request, data=request.POST)
+        if form.is_valid():
+            username = form.cleaned_data.get('username')
+            password = form.cleaned_data.get('password')
+            user = authenticate(username=username, password=password)
+            if user is not None:
+                login(request, user)
+                return redirect("PenaltyManager:dashboard")
             else:
-            # A backend authenticated the credentials
                 return redirect("/")
         else:
-            # No backend authenticated the credentials
             print("Error")
-    return render(request, "base.html", context)
-
-def login_request(request):
-	if request.method == "POST":
-		form = AuthenticationForm(request, data=request.POST)
-		if form.is_valid():
-			username = form.cleaned_data.get('username')
-			password = form.cleaned_data.get('password')
-			user = authenticate(username=username, password=password)
-			if user is not None:
-				login(request, user)
-				return redirect("PenaltyManager:dashboard")
-			else:
-				return redirect("/")
-		else:
-			print("Error")
-	form = AuthenticationForm()
-	return render(request=request, template_name="PenaltyManager/login.html", context={"form":form})
+    form = AuthenticationForm()
+    return render(request=request, template_name="PenaltyManager/login.html", context={"form":form})
 
 def log_out(request):
     logout(request)
